901db4a8759414a3b8dd28aede532b3755ba32e7
Miguel Martínez <miguel-jose.martinez@es.gfi.world>
2021-03-01 15:35:18 +0100

    generate dist

--- src/components/commons/MovieCard/MovieCard.tsx
+++ src/components/commons/MovieCard/MovieCard.tsx
@@ -3,12 +3,17 @@ import { useHistory } from 'react-router-dom';
 import { CoreContext } from 'services/context/coreContext.js';
 import { ROUTER_PAGES } from 'components/Router/constants.js';
 import { IEMovieProps } from 'interfaces/movies.tsx';
-import image from 'assets/images/black-widow.jpg';
+import blackwidow from 'assets/images/black-widow.jpg';
+import jumangi from 'assets/images/jumangi.jpg';
+import onward from 'assets/images/onward.jpg';
 import Rating from '@material-ui/lab/Rating';
+
+const IMAGES = [blackwidow, jumangi, onward];
 
 function MovieCard({ name = '', sortDescription = '', rating = 0, ...props }: IEMovieProps) {
   const { state } = useContext(CoreContext);
   const history = useHistory();
+  const image = IMAGES[Math.floor(Math.random() * 3)];
 
   const goTo = () => {
     history.push({
